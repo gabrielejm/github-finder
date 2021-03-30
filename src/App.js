504d6b27acd3f5235861e1142e4d0645c784b490
Justin Gabriele <gabrielejustin@yahoo.com>
2021-03-30 10:26:46 -0400

    alerts user if empty search

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from "./components/layout/Navbar";
 import "./App.css";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
 import axios from "axios";
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   // Search Github Users
@@ -25,16 +27,23 @@ class App extends Component {
     this.setState({ users: [], loading: false });
   };
 
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+  };
+
   render() {
     const { users, loading } = this.state;
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
